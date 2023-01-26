1a35dc6e1374693dd8d6ae9b9af7b919f426fd6c
Max <Nikolaev.max05@gmail.com>
2023-01-26 13:53:54 +0300

    + wrong logical operator and + new toasts

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -15,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     private TextView Result;
     private EditText Num1, Num2;
 
+    public float parseNumber(EditText CurrentEditText){
+        float number = 0.0f;
+        try {
+            number = Float.parseFloat(CurrentEditText.getText().toString());
+        }
+        catch (NumberFormatException e) {
+            // do nothing here
+        }
+        return number;
+    }
+
     @SuppressLint("CutPasteId")
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,35 +49,61 @@ public class MainActivity extends AppCompatActivity {
 //        if((Num1 != null)||(Num2 != null)){
 
 
+
+
         plus_button.setOnClickListener(v -> {
-            float number1 = Float.parseFloat(Num1.getText().toString());
-            float number2 = Float.parseFloat(Num2.getText().toString());
+//            float number1 = Float.parseFloat(Num1.getText().toString());
+//            float number2 = Float.parseFloat(Num2.getText().toString());
+            float number1 = this.parseNumber(Num1);
+            float number2 = this.parseNumber(Num2);
+
             float Res = number1 + number2;
             Result.setText(String.valueOf(Res));
             toast.show();
         });
 
         minus_button.setOnClickListener(v -> {
-            float number1 = Float.parseFloat(Num1.getText().toString());
-            float number2 = Float.parseFloat(Num2.getText().toString());
+//            float number1 = Float.parseFloat(Num1.getText().toString());
+//            float number2 = Float.parseFloat(Num2.getText().toString());
+            float number1 = this.parseNumber(Num1);
+            float number2 = this.parseNumber(Num2);
+
             float Res = number1 - number2;
             Result.setText(String.valueOf(Res));
             toast.show();
         });
 
         increase_button.setOnClickListener(v -> {
-            float number1 = Float.parseFloat(Num1.getText().toString());
-            float number2 = Float.parseFloat(Num2.getText().toString());
+//            float number1 = Float.parseFloat(Num1.getText().toString());
+//            float number2 = Float.parseFloat(Num2.getText().toString());
+
+            float number1 = this.parseNumber(Num1);
+            float number2 = this.parseNumber(Num2);
+
             float Res = number1 * number2;
             Result.setText(String.valueOf(Res));
             toast.show();
         });
 
         divide_button.setOnClickListener(v -> {
-            float number1 = Float.parseFloat(Num1.getText().toString());
-            float number2 = Float.parseFloat(Num2.getText().toString());
-            float Res = number1 / number2;
-            Result.setText(String.valueOf(Res));
+//            float number1 = Float.parseFloat(Num1.getText().toString());
+//            float number2 = Float.parseFloat(Num2.getText().toString());
+            float number1 = this.parseNumber(Num1);
+            float number2 = this.parseNumber(Num2);
+
+            String result_as_text = "";
+
+            if (number2==0.0f) {
+                result_as_text = "Infinity =)";
+            }
+            else {
+                float Res = number1 / number2;
+                result_as_text = String.valueOf(Res);
+            }
+
+            Result.setText(result_as_text);
+
+
             toast.show();
         });
 //    }
@@ -77,4 +114,4 @@ public class MainActivity extends AppCompatActivity {
 //            toast.show();
 //        }
 
-}}
+    }}
